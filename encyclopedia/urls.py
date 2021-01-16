31c60eb26118a129a0d68254a82c7463cf58beaa
ThoshHub <shamthosh@gmail.com>
2021-01-16 16:04:38 -0500

    Can now create a new page as well

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:entry>", views.entry, name = "wiki"),
-    path("search", views.search, name="search")
+    path("search", views.search, name="search"),
+    path("create", views.create, name="create"), # returns the html page to create a new entry
+    path("create_entry", views.create_entry, name="create_entry") # creates a new entry and returns that page
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-
+from django import forms
 from . import util
 from markdown2 import Markdown
+
+class NewEntryForm(forms.Form):
+    title = forms.CharField(label="Title", widget=forms.TextInput(attrs={'class': "form-control"}))
+    content = forms.CharField(label="Content", widget=forms.Textarea(attrs={'class': "form-control"}))
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -9,7 +13,7 @@ def index(request):
     })
 
 def entry(request, entry):
-    # Get the 
+    # Get the entry
     entry_info_markdown = util.get_entry(entry)
 
     if entry_info_markdown is None:
@@ -21,7 +25,7 @@ def entry(request, entry):
         # print("Info: " + entry_info) # DEBUG
 
         return render(request, "encyclopedia/entry.html", {
-            "entry_title": entry, "entry_info": entry_info
+            "entry_info": entry_info
         })
 
 def search(request):
@@ -31,9 +35,45 @@ def search(request):
         entry_list = util.list_entries()
         results = [entry for entry in entry_list if search_term.upper() in entry.upper()]
 
-        print(str(results))
+        # print(str(results))
         return render(request, "encyclopedia/search.html", {
             "results": results, "query": search_term
         })
     else:
         return render(request, "encyclopedia/error.html") # the user should not be able to make it here as post is the only way to get to this url
+
+def create(request):
+
+    return render(request, "encyclopedia/create.html", {
+        "form": NewEntryForm()
+    })
+
+def create_entry(request):
+    if request.method == "POST":
+        form = NewEntryForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            content = form.cleaned_data["content"]
+            entry_info_check = util.get_entry(title) # check if entry with submitted title exists (to prevent duplicates)
+            
+            if entry_info_check is None: # if it does not exist, then proceed (a duplicate will not be created)
+                print("Title: " + title + ", Content: " + content)
+                util.save_entry(title, content) # create the entry
+                entry_info_markdown = util.get_entry(title)
+                
+                markdowner = Markdown()
+                entry_info = markdowner.convert(entry_info_markdown)
+
+                return render(request, "encyclopedia/entry.html", {
+                    "entry_info": entry_info
+                })
+            else: # a duplicate will be created, so instead  serve an error page
+                print("ERROR PAGE SERVED...")
+                return render(request, "encyclopedia/entry_error.html", {
+                    "title": title
+                })
+        else:
+            return render(request, "encyclopedia/error.html") # the user should not be able to make it here as there is no authentication for invalid strings    
+    else:
+        return render(request, "encyclopedia/error.html") # the user should not be able to make it here as post is the only way to get to this url
+
